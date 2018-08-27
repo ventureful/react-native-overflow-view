043f53dfb13bbea7358f95f0b583db1e49357224
ventureful <hades0122@outlook.com>
2018-08-27 10:05:18 -0400

    add prop for detecting style.overflow
    Removes default use of android native overflow components, and instead transforms into normal View unless style.overflow is set to 'visible'. This eliminates the need of refactor beyond replacing { View } from 'react-native' to View from 'react-native-view-overflow'

--- src/ViewOverflow.android.js
+++ src/ViewOverflow.android.js
@@ -1,11 +1,15 @@
 // @flow
 import * as React from 'react';
+import { View } from 'react-native'
+import _ from 'lodash'
 
 import NativeVieWithoutOverflow from './ViewOverflow-native';
 
 class ViewWithoutOverflow extends React.PureComponent {
   render() {
-    return <NativeVieWithoutOverflow {...this.props} />;
+    if(_.get(this.props, 'style.overflow', 'hidden') == 'visible')
+      return <NativeVieWithoutOverflow {...this.props} />;
+    return <View {...this.props} />
   }
 }
 
